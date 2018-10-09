d8a60d43c43d78c54e37865350918d9c4c824c46
Jeremiah Kellick <jeremiah@jeremiahkellick.com>
2018-10-08 21:56:12 -0700

    Configure devise for API

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
   root to: 'static_pages#root'
+
+  namespace :api, as: nil, defaults: { format: :json } do
+    devise_for :users, controllers: {
+      sessions: 'api/users/sessions',
+      registrations: 'api/users/registrations'
+    }
+  end
 end
