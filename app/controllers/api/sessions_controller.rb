c831292050e3666ea9be06f2be5aeaa7a997ad10
Jeremiah Kellick <jeremiah@jeremiahkellick.com>
2018-10-10 14:42:51 -0700

    Add backend log in and log out

---
+++ app/controllers/api/sessions_controller.rb
@@ -0,0 +1,25 @@
+class Api::SessionsController < ApplicationController
+  def create
+    @user = User.find_by_credentials(
+      params[:user][:email],
+      params[:user][:password]
+    )
+    if @user
+      log_in(@user)
+    else
+      render plain: "Incorrect email or password", status: 401
+    end
+  end
+
+  def destroy
+    if logged_in?
+      log_out
+      render plain: "Logged out successfully"
+    else
+      render(
+        plain: "You must be logged in to log out. Please log in to log out.",
+        status: 401
+      )
+    end
+  end
+end
