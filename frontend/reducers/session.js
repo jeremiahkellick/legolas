20ea38d99fe39029f6318a5ae582e41c357abd34
Jeremiah Kellick <jeremiah@jeremiahkellick.com>
2018-10-10 17:42:25 -0700

    Remove lodash

--- frontend/reducers/session.js
+++ frontend/reducers/session.js
@@ -1,12 +1,11 @@
 import { RECEIVE_CURRENT_USER, LOG_OUT } from '../actions/session';
-import merge from 'lodash/merge';
 
 const _nullSession = { currentUser: null };
 
 const sessionReducer = (state = _nullSession, action) => {
   switch (action.type) {
     case RECEIVE_CURRENT_USER:
-      return merge({}, state, { currentUser: action.currentUser });
+      return Object.assign({}, state, { currentUser: action.currentUser });
     case LOG_OUT:
       return _nullSession;
     default:
