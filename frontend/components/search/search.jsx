3e5c0d41aa8ae2058fb3ff5d1f568dfeffe1ff07
Jeremiah Kellick <jeremiah@jeremiahkellick.com>
2018-10-12 14:25:07 -0700

    Add ugly search results

--- frontend/components/search/search.jsx
+++ frontend/components/search/search.jsx
@@ -1,11 +1,54 @@
 import React from 'react';
+import symbols from './symbols.json';
 import SearchSVG from './search_svg';
 
-const Search = props => (
-  <div className="search-bar">
-    <input type="text" placeholder="Search" />
-    <SearchSVG />
-  </div>
-);
+class Search extends React.Component {
+  constructor(props) {
+    super(props);
+    this.updateQuery = this.updateQuery.bind(this);
+    this.state = { query: '', results: [] };
+  }
+
+  updateQuery(e) {
+    const query = e.target.value;
+    let results = [];
+    if (query.length > 0) {
+      results = symbols.filter(symbol =>
+        this._matches(symbol.symbol, query) || this._matches(symbol.name, query)
+      );
+    }
+    this.setState({ query, results });
+  }
+
+  _matches(string, query) {
+    return string.toLowerCase().startsWith(query.toLowerCase());
+  }
+
+  focus(e) {
+    e.currentTarget.classList.add('focus');
+  }
+
+  blur(e) {
+    e.currentTarget.classList.remove('focus');
+  }
+
+  render () {
+    const results = this.state.results;
+    return (
+      <div onFocus={this.focus} onBlur={this.blur} className="search-bar">
+        <input type="text" onChange={this.updateQuery} placeholder="Search" />
+        <SearchSVG />
+        {
+          results.length > 0 &&
+          <ul>
+            { results.slice(0, 10).map(result =>
+              <li key={result.symbol}>{result.symbol} | {result.name}</li>
+            ) }
+          </ul>
+        }
+      </div>
+    );
+  }
+}
 
 export default Search;
