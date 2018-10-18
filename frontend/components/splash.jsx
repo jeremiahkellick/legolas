3f954beb13cecd5a04d8ec2f6d60e5b6eb7f4271
Jeremiah Kellick <jeremiah@jeremiahkellick.com>
2018-10-18 16:29:01 -0700

    Add animation to splash page

--- frontend/components/splash.jsx
+++ frontend/components/splash.jsx
@@ -1,17 +1,31 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const Splash = props => {
-  document.body.classList.remove('red');
-  return (
-    <section className="splash">
-      <div>
-        <h1>Investing.<br /> Now for the rest of us.</h1>
-        <p>Legolas lets you learn to invest in the stock market for free.</p>
-        <Link className="button" to="/log_in">Log In</Link>
-      </div>
-    </section>
-  );
-};
+class Splash extends React.Component {
+  componentDidMount() {
+    setTimeout(() => $('.step-1').addClass('animated'), 100);
+    setTimeout(() => $('.step-2').addClass('animated'), 800);
+    setTimeout(() => $('.step-3').addClass('animated'), 1400);
+    setTimeout(() => $('.step-4').addClass('animated'), 1800);
+  }
+
+  render () {
+    document.body.classList.remove('red');
+    return (
+      <section className="splash">
+        <div>
+          <h1>
+            <div className="step-1">Investing.</div>
+            <div className="step-2">Now for the rest of us.</div>
+          </h1>
+          <p className="step-3">
+            Legolas lets you learn to invest in the stock market for free.
+          </p>
+          <Link className="button step-4" to="/log_in">Log In</Link>
+        </div>
+      </section>
+    );
+  }
+}
 
 export default Splash;
