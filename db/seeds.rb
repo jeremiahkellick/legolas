a62aa6f45badcd28547f3d6d9b23d3721826a737
Jeremiah Kellick <jeremiah@jeremiahkellick.com>
2018-10-17 17:51:32 -0700

    Fix seed destruction order

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,10 +6,10 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.destroy_all
 Transaction.destroy_all
 Watch.destroy_all
 BalanceChange.destroy_all
+User.destroy_all
 
 User.create!(
   first_name: "Demo",
